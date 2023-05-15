0bde580ab2f651a9194d5236343465ff6120aa23
Gonzalo <gonzalosantacruzariel@gmail.com>
2023-05-14 22:51:27 -0300

    Termino el CRUD

--- Datos/Dproductos.cs
+++ Datos/Dproductos.cs
@@ -31,5 +31,45 @@ namespace Tienda_Api.Datos
             }
             return lista;   
         }
+        public async Task InsertarProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using(var cmd = new SqlCommand("insertarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure; cmd.Parameters.AddWithValue("@descripcion", parametros.descripcion);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync(); 
+                }
+            }
+        }
+        public async Task EditarProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("editarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+        public async Task EliminarProductos(Mproductos parametros)
+        {
+            using (var sql = new SqlConnection(cn.cadenaSQL()))
+            {
+                using (var cmd = new SqlCommand("eliminarProductos", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parametros.id);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
     }
 }
