94c5b3869d78270fed9ff92d980374ef8c4f9303
Gonzalo <gonzalosantacruzariel@gmail.com>
2023-05-14 21:53:45 -0300

    Api terminada

--- Conexion/ConexionBD.cs
+++ Conexion/ConexionBD.cs
@@ -2,5 +2,17 @@
 {
     public class ConexionBD
     {
+        private string connectionString = string.Empty;
+        public ConexionBD()
+        {
+            var Builder = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile
+                ("appsettings.json").Build();
+            connectionString = Builder.GetSection("ConnectionStrings:conexionmaestra").Value;
+        }
+        public string cadenaSQL()
+        {
+            return connectionString;
+        }
     }
 }
--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,6 +1,19 @@
-﻿namespace Tienda_Api.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Tienda_Api.Datos;
+using Tienda_Api.Modelo;
+
+namespace Tienda_Api.Controllers
 {
+    [ApiController]
+    [Route("api/productos")]
     public class ProductosController
     {
-    }
+        [HttpGet]
+        public async Task <ActionResult<List<Mproductos>>> Get()
+        {
+            var funcion = new Dproductos();
+            var lista = await funcion.Mostrarproductos();
+            return lista;
+        }
+    } 
 }
